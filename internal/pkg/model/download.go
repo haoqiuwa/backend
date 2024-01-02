1e12e07955642d400f371fab5d2c3d4d63c62f75
mrl <baseon_life@163.com>
2024-01-02 23:45:30 +0800

    add

--- internal/pkg/model/download.go
+++ internal/pkg/model/download.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"fmt"
 	"time"
 	"wxcloudrun-golang/internal/pkg/db"
 )
@@ -9,10 +10,17 @@ type Download struct {
 	ID          int32     `json:"id" gorm:"primary_key;AUTO_INCREMENT"`
 	OpenID      string    `json:"open_id" gorm:"column:open_id;type:int(11);not null;default:0;comment:'用户id'"`
 	FileID      string    `json:"file_id" gorm:"column:file_id;type:varchar(256);not null;comment:'视频文件id'"`
-	CreatedTime time.Time `json:"created_time" gorm:"column:created_time;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:'创建时间'"`
+	CreatedTime Time      `json:"created_time" gorm:"column:created_time;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:'创建时间'"`
 	UpdatedTime time.Time `json:"updated_time" gorm:"column:updated_time;type:datetime;not null;default:CURRENT_TIMESTAMP;comment:'更新时间'"`
 }
 
+// 自定义 Time 类型
+type Time time.Time
+
+// 实现 MarshalJSON 方法
+func (t Time) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf(`"%s"`, time.Time(t).Format("2006-01-02 15:04:05"))), nil
+}
 func (obj *Download) TableName() string {
 	return "t_download"
 }
