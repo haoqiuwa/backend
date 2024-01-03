af85718026eb2fc7e043e5e247a54c9594bb50af
mrl <baseon_life@163.com>
2024-01-04 00:41:17 +0800

    remove log

--- internal/app/service/venue.go
+++ internal/app/service/venue.go
@@ -10,7 +10,6 @@ import (
 
 func (s *Service) GetVenues(c *gin.Context) {
 	r, err := s.VenueService.GetVenues()
-	log.Println("GetVenues r", r)
 	if err != nil {
 		c.JSON(500, err.Error())
 		return
@@ -22,7 +21,6 @@ func (s *Service) GetVenueDetail(c *gin.Context) {
 	idStr := c.Param("id")
 	id, _ := strconv.Atoi(idStr)
 	r, err := s.VenueService.GetVenueById(int32(id))
-	log.Println("GetVenues r", r)
 	if err != nil {
 		c.JSON(500, err.Error())
 		return
