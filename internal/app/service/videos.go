7175ac1284a00f51166db7b48953ca45bc1d3116
mrl <baseon_life@163.com>
2024-01-07 18:45:57 +0800

    增加下载状态查询

--- internal/app/service/videos.go
+++ internal/app/service/videos.go
@@ -14,6 +14,43 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type VideoClipsRes struct {
+	ID             int32     `json:"id"`             // 自增主键
+	FilePath       string    `json:"file_path"`      // 视频地址
+	Team           string    `json:"team"`           // 队
+	CourtUUID      string    `json:"court_uuid"`     // 场次uuid
+	HoverImgPath   string    `json:"hover_img_path"` // 封面图地址
+	CreateTime     time.Time `json:"create_time"`    // 创建时间
+	UpdateTime     time.Time `json:"update_time"`    // 更新时间
+	VideoType      int32     `json:"video_type"`     // 视频类型  1 集锦 2 ai视频
+	Time           int32     `json:"time"`           // 视频时长
+	TimeRange      string    `json:"time_range"`
+	DownloadStatus bool      `json:"download_status"`
+}
+
+type VideoRes struct {
+	UUID           string    `json:"uuid"`
+	ID             int32     `json:"id"`
+	FilePath       string    `json:"file_path"`
+	Date           int32     `json:"date"`
+	Time           int32     `json:"time"`
+	Type           int32     `json:"type"`
+	VenueId        int32     `json:"venue_id"`
+	Court          int32     `json:"court"`
+	Hour           int32     `json:"hour"`
+	FileName       string    `json:"file_name"`
+	VideoName      string    `json:"video_name"`
+	FileType       int32     `json:"file_type"`
+	CreatedTime    time.Time `json:"created_time"`
+	UpdatedTime    time.Time `json:"updated_time"`
+	StartTime      int64     `json:"start_time"`
+	EndTime        int64     `json:"end_time"`
+	TeamAImgPath   string    `json:"team_a_img_path"`
+	TeamBImgPath   string    `json:"team_b_img_path"`
+	HoverImgPath   string    `json:"hover_img_path"`
+	DownloadStatus bool      `json:"download_status"`
+}
+
 type TimeRangeRes struct {
 	VenueId        int32 `json:"venue_id"`
 	CourtId        int32 `json:"court_id"`
@@ -306,13 +343,41 @@ func (s *Service) GetAiVideos(c *gin.Context) {
 
 // 获取集锦视频
 func (s *Service) GetHighlightsVideos(c *gin.Context) {
+	openID := c.GetHeader("X-WX-OPENID")
 	uuid := c.Param("uuid")
 	data, err := s.EventService.GetHighlightsVideos(uuid)
 	if err != nil {
 		c.JSON(500, err.Error())
 		return
 	}
-	c.JSON(200, resp.ToStruct(data, err))
+	rs := s.FetchDownloadStatusClips(data, openID)
+	c.JSON(200, resp.ToStruct(rs, err))
+}
+
+func (s *Service) FetchDownloadStatusClips(datas []model.VideoClips, openId string) []*VideoClipsRes {
+	results := make([]*VideoClipsRes, 0)
+	for _, v := range datas {
+		rr := &VideoClipsRes{}
+		rr.ID = v.ID
+		rr.FilePath = v.FilePath
+		rr.Team = v.Team
+		rr.CourtUUID = v.CourtUUID
+		rr.HoverImgPath = v.HoverImgPath
+		rr.CreateTime = v.CreateTime
+		rr.UpdateTime = v.UpdateTime
+		rr.VideoType = v.VideoType
+		rr.Time = v.Time
+		rr.TimeRange = v.TimeRange
+		d, err := s.DownloadRecordService.GetByOpenIdResourceIdAndresourceType(openId, v.ID, 30)
+		if nil != err {
+			log.Println("fetchDownloadStatus err", err)
+		}
+		if nil != d && d.ID > 0 {
+			rr.DownloadStatus = true
+		}
+		results = append(results, rr)
+	}
+	return results
 }
 
 // 获取视频图册
@@ -420,6 +485,11 @@ func (s *Service) TimeRangeV1(c *gin.Context) {
 }
 
 func (s *Service) GetVideoList(c *gin.Context) {
+	openID := c.GetHeader("X-WX-OPENID")
+	if openID == "" {
+		c.JSON(400, "请先登录")
+		return
+	}
 	dateStr := c.Query("date")
 	date, err := strconv.Atoi(dateStr)
 	if err != nil {
@@ -450,7 +520,42 @@ func (s *Service) GetVideoList(c *gin.Context) {
 		return
 	}
 	data, err := s.EventService.GetVideoList(int32(date), court.CourtCode, int32(hour), int32(venueId))
-	c.JSON(200, resp.ToStruct(data, err))
+	rs := s.FetchDownloadStatusVideo(data, openID)
+	c.JSON(200, resp.ToStruct(rs, err))
+}
+func (s *Service) FetchDownloadStatusVideo(datas []*model.Video, openId string) []*VideoRes {
+	results := make([]*VideoRes, 0)
+	for _, v := range datas {
+		rr := &VideoRes{}
+		rr.UUID = v.UUID
+		rr.ID = v.ID
+		rr.FilePath = v.FilePath
+		rr.Date = v.Date
+		rr.Time = v.Time
+		rr.Type = v.Type
+		rr.VenueId = v.VenueId
+		rr.Court = v.Court
+		rr.Hour = v.Hour
+		rr.FileName = v.FileName
+		rr.VideoName = v.VideoName
+		rr.FileType = v.FileType
+		rr.CreatedTime = v.CreatedTime
+		rr.UpdatedTime = v.UpdatedTime
+		rr.StartTime = v.StartTime
+		rr.EndTime = v.EndTime
+		rr.TeamAImgPath = v.TeamAImgPath
+		rr.TeamBImgPath = v.TeamBImgPath
+		rr.HoverImgPath = v.HoverImgPath
+		d, err := s.DownloadRecordService.GetByOpenIdResourceIdAndresourceType(openId, v.ID, 10)
+		if nil != err {
+			log.Println("fetchDownloadStatus err", err)
+		}
+		if nil != d && d.ID > 0 {
+			rr.DownloadStatus = true
+		}
+		results = append(results, rr)
+	}
+	return results
 }
 
 func (s *Service) GetVideoDetails(c *gin.Context) {
