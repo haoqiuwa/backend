c87d5a7ddfaae936733e1206617667ac94e890ba
mrl <baseon_life@163.com>
2024-01-13 17:23:44 +0800

    fetch down status

--- internal/app/service/videos.go
+++ internal/app/service/videos.go
@@ -289,7 +289,7 @@ func (s *Service) GetMatchHighlights(c *gin.Context) {
 	}
 	for _, v := range data.VideoSeries {
 		for _, vv := range v.Videos {
-			d, err := s.DownloadRecordService.GetByOpenIdResourceIdAndresourceType(openID, vv.Id, 40)
+			d, err := s.DownloadRecordService.GetByOpenIdResourceIdAndresourceType(openID, vv.Id, 50)
 			if nil != err {
 				log.Println("fetchDownloadStatus err", err)
 			}
